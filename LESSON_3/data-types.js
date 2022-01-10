858b330545b7acb9e4cf7bb371636df89dbf9dcf
Stan <s2000ProMax@gmail.com>
2022-01-10 18:33:51 +0300

    Lesson_3

---
+++ LESSON_3/data-types.js
@@ -0,0 +1,23 @@
+const number = 125
+console.log('Number:', number, typeof number)
+
+const string = 'String'
+console.log('String:', string, typeof string)
+
+const isBoolean = true
+console.log('Boolean:', isBoolean, typeof isBoolean)
+
+const nullType = null
+console.log('Null:', nullType, typeof nullType)
+
+let undefinedType
+console.log('Undefined:', undefinedType, typeof undefinedType)
+
+const object = {}
+console.log('Object:', object, typeof object)
+
+const bigintType = 500n
+console.log('BigInt:', bigintType, typeof bigintType)
+
+const symbolType = Symbol('key')
+console.log('Symbol:', symbolType, typeof symbolType)
