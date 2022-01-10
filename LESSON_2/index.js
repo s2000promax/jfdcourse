a9b4c0f4b2005562014e53d1bd2c57d2f023363e
Stan <s2000ProMax@gmail.com>
2022-01-10 16:27:13 +0300

    Lesson_2

---
+++ LESSON_2/index.js
@@ -0,0 +1,18 @@
+//“JavaScript код с созданием переменных” с помощью способа “//”
+
+/*
+Автор:
+Stanislav Venger
+*/
+
+const myName = 'Stanislav'
+const mySurname = 'Venger'
+const myFavoriteDrink = 'Juice'
+const myFavoriteAnimal = 'Elephant'
+const myFavoriteProgrammingLanguage = 'JavaScript' //it's not a joke
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
