f0d71d9d62a1ad6701ab9f7075459377157322cd
Stan <s2000ProMax@gmail.com>
2022-03-21 14:25:02 +0300

    02-components: useState

---
+++ 02-components/src/components/counter.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  console.log('###-counter', counter);
+
+  const formatCount = () => {
+    return counter === 0 ? 'empty' : counter;
+
+  }
+
+  const getBadgeClasses = () => {
+    return counter === 0 ? 'badge m-2 btn-warning' : 'badge m-2 btn-primary';
+  }
+
+  const handleIncrement = () => {
+    setCounter((prevState) => prevState + 1);
+  }
+
+  const handleDecrement = () => {
+    setCounter((prevState) => prevState - 1);
+  }
+
+  return (
+    <React.Fragment>
+      <span className={getBadgeClasses()}>{formatCount()}</span>
+      <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
+      <button className='btn btn-primary btn-sm m-2' onClick={handleDecrement}>-</button>
+    </React.Fragment>
+  );
+}
+
+export default Counter;
