4f55d5f1b6e7bb7bc6bdc9b96accba420a1976b7
Stan <s2000ProMax@gmail.com>
2022-03-21 14:36:10 +0300

    02-components: render from array

--- 02-components/src/components/counter.jsx
+++ 02-components/src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
+  const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
 
   console.log('###-counter', counter);
 
@@ -22,8 +23,17 @@ const Counter = () => {
     setCounter((prevState) => prevState - 1);
   }
 
+  const handleTagChange = () => {
+    setTags(['tag4', 'tag5']);
+  }
+
   return (
     <React.Fragment>
+      <ul>
+        {tags.map((tag) => (
+          <li key={tag} className='btn btn-primary btn-sm m-2' onClick={handleTagChange}>{tag}</li>
+        ))}
+      </ul>
       <span className={getBadgeClasses()}>{formatCount()}</span>
       <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
       <button className='btn btn-primary btn-sm m-2' onClick={handleDecrement}>-</button>
