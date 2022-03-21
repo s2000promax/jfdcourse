20e669bd3f555f63a21fb5b9ab94dc7e521af732
Stan <s2000ProMax@gmail.com>
2022-03-21 15:00:25 +0300

    02-components: conditional rendering

--- 02-components/src/components/counter.jsx
+++ 02-components/src/components/counter.jsx
@@ -22,22 +22,29 @@ const Counter = () => {
   }
 
   const handleTagChange = (id) => {
-    // setTags(['tag4', 'tag5']);
-    console.log('###-id', id);
     setTags(prevState => prevState.filter(tag => tag !== id));
+  }
+
+  const renderTags = () => {
+    return !!tags.length && tags.map((tag) => (
+      <li
+        key={tag}
+        className='btn btn-primary btn-sm m-2'
+        onClick={() => handleTagChange(tag)}>{tag}
+      </li>
+    ));
+  }
+
+  if (!!tags.length) {
+    return (
+      <ul>
+        {renderTags()}
+      </ul>
+    );
   }
 
   return (
     <React.Fragment>
-      <ul>
-        {tags.map((tag) => (
-          <li
-            key={tag}
-            className='btn btn-primary btn-sm m-2'
-            onClick={() => handleTagChange(tag)}>{tag}
-          </li>
-        ))}
-      </ul>
       <span className={getBadgeClasses()}>{formatCount()}</span>
       <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
       <button className='btn btn-primary btn-sm m-2' onClick={handleDecrement}>-</button>
