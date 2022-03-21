c56c639fb38a37bb4895d52cd57e060a9ad2bdb8
Stan <s2000ProMax@gmail.com>
2022-03-21 14:43:36 +0300

    02-components: delete selected element

--- 02-components/src/components/counter.jsx
+++ 02-components/src/components/counter.jsx
@@ -4,8 +4,6 @@ const Counter = () => {
   const [counter, setCounter] = useState(0);
   const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
 
-  console.log('###-counter', counter);
-
   const formatCount = () => {
     return counter === 0 ? 'empty' : counter;
 
@@ -23,15 +21,21 @@ const Counter = () => {
     setCounter((prevState) => prevState - 1);
   }
 
-  const handleTagChange = () => {
-    setTags(['tag4', 'tag5']);
+  const handleTagChange = (id) => {
+    // setTags(['tag4', 'tag5']);
+    console.log('###-id', id);
+    setTags(prevState => prevState.filter(tag => tag !== id));
   }
 
   return (
     <React.Fragment>
       <ul>
         {tags.map((tag) => (
-          <li key={tag} className='btn btn-primary btn-sm m-2' onClick={handleTagChange}>{tag}</li>
+          <li
+            key={tag}
+            className='btn btn-primary btn-sm m-2'
+            onClick={() => handleTagChange(tag)}>{tag}
+          </li>
         ))}
       </ul>
       <span className={getBadgeClasses()}>{formatCount()}</span>
