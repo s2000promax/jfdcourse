fd3ff836f8cd0d826ff7bb9c4125a93c1d7e6233
Stan <s2000ProMax@gmail.com>
2022-07-26 20:57:40 +0300

    4-Web server. Extended task

--- 4-WebServer/public/app.js
+++ 4-WebServer/public/app.js
@@ -1,24 +1,53 @@
 document.addEventListener('click', (event) => {
-  if (event.target.dataset.type === 'edit') {
+  if (event.target.dataset.type === 'update') {
     const id = event.target.dataset.id;
-    const currentTitle = event.target.closest('li').childNodes[0].textContent.trim();
+    const newTitle = event.target.closest('li').childNodes[0].value;
+    const currentNode = event.target.closest('li');
 
-    const newTitle = prompt('Enter new title', currentTitle);
     if (!!newTitle) {
       update(id, newTitle).then(()=> {
-        event.target.closest('li').childNodes[0].textContent = newTitle;
+        currentNode.innerHTML = `${newTitle}
+                    <div>
+                        <button class="btn btn-primary" data-type="edit" data-id=${id} >Edit</button>
+                        <div class="btn btn-danger" data-type="remove" data-id=${id} >&times;</div>
+                    </div>`;
       });
     }
   }
 
-  if (event.target.dataset.type === 'remove') {
-    const id = event.target.dataset.id;
-    console.log('remove', id);
+    if (event.target.dataset.type === 'cancel') {
+      const id = event.target.dataset.id;
+      const currentTitle = event.target.closest('li').childNodes[0].value;
+      const currentNode = event.target.closest('li');
+      currentNode.innerHTML = `${currentTitle}
+                    <div>
+                        <button class="btn btn-primary" data-type="edit" data-id=${id} >Edit</button>
+                        <div class="btn btn-danger" data-type="remove" data-id=${id} >&times;</div>
+                    </div>`;
+    }
 
-    remove(id).then(() => {
-      event.target.closest('li').remove();
-    });
-  }
+    if (event.target.dataset.type === 'edit') {
+      const id = event.target.dataset.id;
+      const currentTitle = event.target.closest('li').childNodes[0].textContent.trim();
+
+      const currentNode = event.target.closest('li');
+      console.log(currentNode);
+
+      currentNode.innerHTML = `<input type="text" value=${currentTitle}>
+                    <div>
+                        <button class="btn btn-success" data-type="update" data-id=${id} >Update</button>
+                        <button class="btn btn-danger" data-type="cancel" data-id=${id} >Cancel</button>
+                    </div>`;
+    }
+
+    if (event.target.dataset.type === 'remove') {
+      const id = event.target.dataset.id;
+      console.log('remove', id);
+
+      remove(id).then(() => {
+        event.target.closest('li').remove();
+      });
+    }
 });
 
 async function remove(id) {
@@ -35,4 +64,4 @@ async function update(id, title) {
     },
     body: JSON.stringify({ title })
   });
-}
+};
